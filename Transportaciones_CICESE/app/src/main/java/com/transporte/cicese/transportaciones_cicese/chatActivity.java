467026c5ed4ba994ed1161c7cf75607195ef4579
efrainlozano <efrain.lozano@uabc.edu.mx>
2017-11-28 23:19:50 -0800

    chatCommit

--- Transportaciones_CICESE/app/src/main/java/com/transporte/cicese/transportaciones_cicese/chatActivity.java
+++ Transportaciones_CICESE/app/src/main/java/com/transporte/cicese/transportaciones_cicese/chatActivity.java
@@ -9,15 +9,18 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ScrollView;
 
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.transporte.cicese.transportaciones_cicese.adapters.customSpinnerAdapter;
 import com.transporte.cicese.transportaciones_cicese.funciones.funcionesGeneradoras;
 
 import org.json.JSONArray;
@@ -52,15 +55,16 @@ import javax.net.ssl.X509TrustManager;
 
 public class chatActivity extends AppCompatActivity {
 
-    LinearLayout layout;
-    EditText messageArea;
-    String mensaje, miNombre,fecha,hora,resource;
-    int miId, idServicio;
-    ImageView sendButton;
-    ScrollView scrollView;
-    String title = "";
-    funcionesGeneradoras fG;
-    ArrayList fields,values;
+    private LinearLayout layout;
+    private EditText messageArea;
+    private String mensaje, miNombre,fecha,hora,resource;
+    private int miId, idServicio;
+    private ImageView sendButton;
+    private ScrollView scrollView;
+    private String title = "";
+    private ArrayList fields,values;
+    private Spinner serviciosSpinner,solicitudesSpinner;
+    private JSONArray serviciosResult,solicitudesResult;
 
 
   /* String recuperamos_variable_string;// = getIntent().getExtras().getString("textoR_1").toString();
@@ -70,19 +74,17 @@ public class chatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
+        serviciosSpinner = (Spinner) findViewById(R.id.serviciosSpinner);
+        solicitudesSpinner = (Spinner) findViewById(R.id.solicitudesSpinner);
 
         //Del texto rapido
         /*Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
-            bool = getIntent().getExtras().getBoolean("selecto");
-        }
+            bool = getIntent().getExtras().getBoolean("selecto");      }
 */
-        fG= new funcionesGeneradoras(getApplication());
         title="Chat";
         getSupportActionBar().setTitle(title);
-
         SharedPreferences settings = getSharedPreferences("prefs", MODE_PRIVATE);
         sendButton = (ImageView)findViewById(R.id.sendButton);
         messageArea = (EditText)findViewById(R.id.messageArea);
@@ -90,21 +92,18 @@ public class chatActivity extends AppCompatActivity {
         scrollView = (ScrollView)findViewById(R.id.scrollView);
         miId = settings.getInt("idUsuario",0); //AQUI DEBERA DE IR AL SHAREDPREFERENCE DEL ID DEL USUARIO LOGUEADO
         miNombre=settings.getString("nUsuario","Default");
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////
-
-        idServicio=1;//El identificador se obtiene desde el activity de servicios una vez haya seleccionado uno
-
-
-        /////////////////////////////////////////////////////////////////////////////////////////
-
-
+        String tipoUsuario = settings.getString("tipoUsuario","Default");
+        switch (tipoUsuario) {
+            case "p":
+                serviciosSpinner.setEnabled(true);
+                new chatActivity.obtenerServicios().execute();
+                break;
+            default:
+                solicitudesSpinner.setVisibility(View.VISIBLE);
+                new chatActivity.obtenerSolicitudes().execute();
+                break;
+        }
         //trustAllCertificates();
-        fields=new ArrayList(Arrays.asList("id"));
-        values=new ArrayList(Arrays.asList(idServicio));
-        resource ="gmensajes";
-        new chatActivity.ConsultarDatos().execute();
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,11 +112,57 @@ public class chatActivity extends AppCompatActivity {
                     recuperamos_variable_string = getIntent().getExtras().getString("textoR_1");
                     messageArea.setText(recuperamos_variable_string);
                 }else{*/
-
-
                 mensaje = messageArea.getText().toString();//}
                 new chatActivity.SendPostRequest().execute(getString(R.string.URI)+"/amensaje");
                 messageArea.setText(null);
+            }
+        });
+
+        solicitudesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    JSONObject jObject = null;
+                    try {
+                        serviciosSpinner.setEnabled(true);
+                        jObject = new JSONObject(solicitudesResult.get(position - 1).toString());
+                        serviciosSpinner.setAdapter(null);
+                        new chatActivity.obtenerServicios().execute(jObject.getString("id_solicitud"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    serviciosSpinner.setEnabled(false);
+                }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        serviciosSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i != 0) {
+                    JSONObject jObject = null;
+                    try {
+                        jObject = new JSONObject(serviciosResult.get(i-1).toString());
+                        //idServicio = jObject.getInt("id_servicio");
+                        idServicio=jObject.getInt("id_servicio");
+                        layout.removeAllViews();
+                        new chatActivity.ConsultarDatos().execute();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
     }
@@ -128,6 +173,7 @@ public class chatActivity extends AppCompatActivity {
         protected ArrayList doInBackground(String... urls) {
             Time today = new Time(Time.getCurrentTimezone());
             today.setToNow();
+            funcionesGeneradoras fG= new funcionesGeneradoras(getApplication());
             try {
                 URL url = new URL(urls[0]); // here is your URL path
                 fecha = today.year+"-"+(today.month+1)+"-"+today.monthDay;
@@ -157,34 +203,13 @@ public class chatActivity extends AppCompatActivity {
         }
     }
 
-    public String getPostDataString(JSONObject params) throws Exception {
-
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-
-        Iterator<String> itr = params.keys();
-
-        while(itr.hasNext()){
-
-            String key= itr.next();
-            Object value = params.get(key);
-
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(key, "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
-
-        }
-        return result.toString();
-    }
-
     private class ConsultarDatos extends AsyncTask<String, Void, ArrayList> {
         // Do some validation here
         protected ArrayList doInBackground(String... urls) {
+            funcionesGeneradoras fG= new funcionesGeneradoras(getApplication());
+            fields=new ArrayList(Arrays.asList("id"));
+            values=new ArrayList(Arrays.asList(idServicio));
+            resource ="gmensajes";
             return fG.functionGetRequest(resource,fields,values);
         }
         @Override
@@ -245,4 +270,108 @@ public class chatActivity extends AppCompatActivity {
         layout.addView(textView);
         scrollView.fullScroll(View.FOCUS_DOWN);
     }
+
+    private class obtenerServicios extends AsyncTask<String, Void, ArrayList> {
+        // Do some validation here
+
+        protected ArrayList doInBackground(String... urls) {
+            // Do some validation here
+            funcionesGeneradoras fG= new funcionesGeneradoras(getApplication());
+            SharedPreferences settings = getSharedPreferences("prefs", MODE_PRIVATE);
+            String tipoUsuario = settings.getString("tipoUsuario", "Default");
+            //List<String> myList = new ArrayList<String>(Arrays.asList(arr.split(",")));
+            ArrayList fields = new ArrayList(Arrays.asList("id"));
+            ArrayList result = new ArrayList();
+            ArrayList values;
+            switch (tipoUsuario) {
+                case "p":
+                    String solicitudes = settings.getString("solicitudes", "Default")
+                            .replace("[", "").replace("]", "");
+                    values = new ArrayList(Arrays.asList(solicitudes));
+                    result = fG.functionGetRequest("gservicios", fields, values);
+                    break;
+                default:
+                    values = new ArrayList(Arrays.asList(urls[0]));
+                    result = fG.functionGetRequest("gservicios", fields, values);
+                    break;
+
+            }
+            return result;
+            //Funcion que regresa JSON con todos los datos de los pasajeros/invitados
+        }
+
+        protected void onPostExecute(ArrayList result) {
+            if ((Integer) result.get(0) == HttpURLConnection.HTTP_OK) {
+                String[] serviciosArray;
+                customSpinnerAdapter spinnerAdapterServicios;
+                String data = result.get(1).toString();
+                try {
+                    serviciosResult = new JSONArray(data);
+                    serviciosArray = new String[serviciosResult.length() + 1];
+                    serviciosArray[0] = "Seleccione un servicio de la lista";
+                    for (int i = 0; i < serviciosResult.length(); i++) {
+                        JSONObject oneObject = new JSONObject(serviciosResult.get(i).toString());
+                        serviciosArray[i + 1] = oneObject.getString("fecha_encuentro") +
+                                " - " + oneObject.getString("descripcion_lugar_encuentro").substring(0,14)+"..." +
+                                " -> " + oneObject.getString("descripcion_lugar_destino").substring(0,14)+"...";
+                    }
+                    spinnerAdapterServicios = new customSpinnerAdapter(chatActivity.this, serviciosArray, null);
+                    serviciosSpinner.setAdapter(spinnerAdapterServicios);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "No hay servicios regitrados a esta solicitud", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+    private class obtenerSolicitudes extends AsyncTask<String, Void, ArrayList> {
+        // Do some validation here
+
+        protected ArrayList doInBackground(String... urls) {
+            // Do some validation here
+            funcionesGeneradoras fG= new funcionesGeneradoras(getApplication());
+            SharedPreferences settings = getSharedPreferences("prefs", MODE_PRIVATE);
+            String tipoUsuario = settings.getString("tipoUsuario", "Default");
+            String arr = settings.getString("solicitudes", "Default").replace("[", "")
+                    .replace("]", "").replace(" ", "");
+            //List<String> myList = new ArrayList<String>(Arrays.asList(arr.split(",")));
+            ArrayList fields = new ArrayList(Arrays.asList("idSolicitudes"));
+            ArrayList values = new ArrayList(Arrays.asList(/*myList.get(0)*/arr));
+            return fG.functionGetRequest("gdatasolicituds", fields, values);
+
+
+            //Funcion que regresa JSON con todos los datos de los pasajeros/invitados
+        }
+
+        protected void onPostExecute(ArrayList result) {
+            if ((Integer) result.get(0) == HttpURLConnection.HTTP_OK) {
+                String[] solicitudesArray;
+                customSpinnerAdapter spinnerAdapterServicios;
+                String data = result.get(1).toString();
+                try {
+                    solicitudesResult = new JSONArray(data);
+                    solicitudesArray = new String[solicitudesResult.length() + 1];
+                    solicitudesArray[0] = "Seleccione una solicitud de la lista";
+                    for (int i = 0; i < solicitudesResult.length(); i++) {
+                        JSONObject oneObject = new JSONObject(solicitudesResult.get(i).toString());
+                        solicitudesArray[i + 1] =
+                                oneObject.getString("id_folio") + " - " +
+                                        oneObject.getString("nombre_pasajero");
+                    }
+                    spinnerAdapterServicios = new customSpinnerAdapter
+                            ( chatActivity.this, solicitudesArray, null);
+                    solicitudesSpinner.setAdapter(spinnerAdapterServicios);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "No hay servicios regitrados a esta solicitud",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
 }
+
